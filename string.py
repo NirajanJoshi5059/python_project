6ad94d40af9a93e26a9b30a81a07c0a34086cafe
NirajanJoshi5059 <82226297+NirajanJoshi5059@users.noreply.github.com>
2022-03-24 16:14:59 +0545

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+title="Python for Begginers"
+name='Nirajan Joshi'
+course="Python Full Course"
+
+print(len(title))
+print(name)
+print(title.title()) #It does all capital of initial letter
+print(course.count("P"))
+print(name.upper())
+print("Python" in title)#Gives us boolean value
+
+
+#Formatted String
+
+name="Saurav"
+print(f"{name} wants to be a PROGRAMMER.")
